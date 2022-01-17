49f4161003ecb1cab1810d48899f9c6e6c0965b9
Jordan.#2139 <65438497+Jordan2139@users.noreply.github.com>
2022-01-17 14:19:39 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const https = require('https');
+
+class monkey {
+    constructor(client, debug, badlink) {
+        this.client = client;
+        this.debug = debug;
+        this.badlink = badlink;
+    }
+
+    async checkLink(message) { // The event that will see is the msg contains a no no link
+        https.get('https://jordan2139.me/api/scamlinks.txt', (result) => {
+            var body = '';
+            result.on('data', (chunk) => {
+                body += chunk
+            })
+            result.on('end', () => {
+                body.forEach(async link => {
+                    console.log(link)
+                    if (message.content.includes(link)) {
+                        message.delete().catch(e => {})
+                        message.channel.send({ content: `**You're message was deleted because it contained a restricted link:** ||${link}||` })
+                    }
+                })
+            })
+        })
+    }
+}
+
+module.exports = monkey;
